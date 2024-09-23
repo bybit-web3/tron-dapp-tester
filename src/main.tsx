e55fec4771bd3418432f1d678499b8999bf69417
potter.zhang <potter.zhang@bybit.com>
2024-09-23 10:00:35 +0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks';
+import ReactDOM from 'react-dom/client';
+import '@tronweb3/tronwallet-adapter-react-ui/style.css';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+import { App } from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import vConsole from 'vconsole'
+new vConsole();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App></App>
+            <Toaster />
+        </ThemeProvider>
+    </React.StrictMode>
+);
